6074bba77493b17c358c91975d731716758337b2
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-04-05 14:38:37 +0700

    add CheckTransaction method to CheckoutService for transaction status retrieval

---
+++ pkg/payway/check_transaction.go
@@ -0,0 +1,38 @@
+package payway
+
+import (
+	"context"
+	"fmt"
+)
+
+type CheckTransactionResponse struct {
+	TranID    string    `json:"tran_id"`
+	Status    string    `json:"status"`
+	APIStatus APIStatus `json:"status_code"`
+}
+
+func (s *CheckoutService) CheckTransaction(ctx context.Context, tranID string) (*CheckTransactionResponse, error) {
+	if tranID == "" {
+		return nil, fmt.Errorf("payway/checkout: tranID is required")
+	}
+
+	reqTime := NowReqTime()
+	params := map[string]string{
+		"req_time":    reqTime,
+		"merchant_id": s.cfg.MerchantID,
+		"tran_id":     tranID,
+	}
+
+	generatedHash, err := s.hasher.Generate(params)
+	if err != nil {
+		return nil, err
+	}
+	params["hash"] = generatedHash
+
+	var resp CheckTransactionResponse
+	if err := s.http.postJSON(ctx, pathCheckTransaction, params, &resp); err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
