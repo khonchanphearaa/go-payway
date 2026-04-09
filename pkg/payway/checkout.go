ffb78c483e8a877cad00194cbf1d51bb57a44cf6
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-04-09 13:51:12 +0700

    refactor: remove commented-out validation function in CheckoutService

--- pkg/payway/checkout.go
+++ pkg/payway/checkout.go
@@ -160,23 +160,3 @@ func (s *CheckoutService) GetExchangeRate(ctx context.Context) (*ExchangeRateRes
 
 	return &resp, nil
 }
-
-// // Validation amount
-// func (s *CheckoutService) validatePurchase(req *PurchaseRequest) error {
-// 	if req.TransactionID == "" {
-// 		return fmt.Errorf("payway/checkout: TransactionID is required")
-// 	}
-// 	if req.Amount <= 0 {
-// 		return fmt.Errorf("payway/checkout: Amount must be greater than 0")
-// 	}
-// 	if req.Currency == "" {
-// 		return fmt.Errorf("payway/checkout: Currency is required")
-// 	}
-// 	if req.ReturnURL == "" {
-// 		return fmt.Errorf("payway/checkout: ReturnURL is required")
-// 	}
-// 	if len(req.Items) == 0 {
-// 		return fmt.Errorf("payway/checkout: at least one Item is required")
-// 	}
-// 	return nil
-// }
